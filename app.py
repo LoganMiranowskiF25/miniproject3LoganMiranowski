ffa3fae2e9d0adcba17fe8c547f32002897195d8
logmiranowski <logmiranowski@gmail.com>
2026-03-22 20:24:56 -0500

    Connected flask app to index.html template

--- app.py
+++ app.py
@@ -2,3 +2,15 @@
 # Class: INF360
 # Project: Flask Web App
 
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Route for the home page
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    print("Starting the Flask app...")
+    app.run(debug=True)
